baed4bf99b3423ea2a1b0f8074b67e8aa902d682
ignurof <49594591+ignurof@users.noreply.github.com>
2021-01-17 23:33:45 +0100

    fix for bug:Obstacles spawning too fast & refactoring

--- FlyingDog/Assets/Scripts/MoveObstacle.cs
+++ FlyingDog/Assets/Scripts/MoveObstacle.cs
@@ -4,9 +4,33 @@ using UnityEngine;
 
 public class MoveObstacle : MonoBehaviour
 {
+    public string position;
+
+    private void Start()
+    {
+        switch (position)
+        {
+            case "bot":
+                Destroy(gameObject, 5);
+                break;
+            case "top":
+                transform.Rotate(0, 0, -180);
+                Destroy(gameObject, 5);
+                break;
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(-1 * 6 * Time.deltaTime, 0, 0);
+        switch (position)
+        {
+            case "bot":
+                transform.Translate(-1 * 6 * Time.deltaTime, 0, 0);
+                break;
+            case "top":
+                transform.Translate(1 * 6 * Time.deltaTime, 0, 0);
+                break;
+        }
     }
 }
--- FlyingDog/Assets/Scripts/ObstacleSpawner.cs
+++ FlyingDog/Assets/Scripts/ObstacleSpawner.cs
@@ -9,32 +9,46 @@ public class ObstacleSpawner : MonoBehaviour
     public GameObject prefab;
     private Vector3 bottomPos;
     private Vector3 topPos;
+
+    private bool waitingSpawn;
     
     // Start is called before the first frame update
     void Start()
     {
         bottomPos = bottomSpawner.gameObject.transform.position;
         topPos = topSpawner.gameObject.transform.position;
+        waitingSpawn = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        switch (RandomSpawnerGenerator())
-        {
-            case 1:
-                Instantiate(prefab, bottomPos, Quaternion.Euler(0, 0, 0));
-                break;
-            case 2:
-                Instantiate(prefab, topPos, Quaternion.Euler(0, 0, 0));
-                break;
-        }
+        // Check the random result and spawn obstacle
+        if (waitingSpawn)
+            StartCoroutine(RandomSpawnerGenerator());
+        else
+            Debug.Log("Waiting on SpawnTimer");
     }
 
-    int RandomSpawnerGenerator()
+    // Spawn Timer
+    IEnumerator RandomSpawnerGenerator()
     {
+        waitingSpawn = false;
         int result;
         result = Random.Range(1, 3);
-        return result;
+
+        switch (result)
+        {
+            case 1:
+                var bottomObj = Instantiate(prefab, bottomPos, Quaternion.Euler(0, 0, 0));
+                bottomObj.GetComponent<MoveObstacle>().position = "bot";
+                break;
+            case 2:
+                var topObj = Instantiate(prefab, topPos, Quaternion.Euler(0, 0, 0));
+                topObj.GetComponent<MoveObstacle>().position = "top";
+                break;
+        }
+        yield return new WaitForSeconds(2);
+        waitingSpawn = true;
     }
 }
