cb7d326617a596caf9545b225e32c57de73e3f6b
ignurof <49594591+ignurof@users.noreply.github.com>
2021-01-18 18:19:38 +0100

    being work on pausemenu

---
+++ FlyingDog/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    private bool isPlaying;
+
+    private void Start()
+    {
+        isPlaying = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPlaying)
+            {
+                isPlaying = false;
+                Debug.Log("Pause menu");
+            }
+            else
+            {
+                isPlaying = true;
+                Debug.Log("Unpause");
+            } 
+        }
+    }
+}
