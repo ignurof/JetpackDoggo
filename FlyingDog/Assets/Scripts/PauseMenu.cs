a97b16899259fe5c5c59be7b5e6a84c55380e8f4
ignurof <49594591+ignurof@users.noreply.github.com>
2021-01-18 18:52:44 +0100

    base pausemenu completed

--- FlyingDog/Assets/Scripts/PauseMenu.cs
+++ FlyingDog/Assets/Scripts/PauseMenu.cs
@@ -1,14 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class PauseMenu : MonoBehaviour
 {
     private bool isPlaying;
 
+    private GameObject pausePanel;
+    private GameObject pauseBtn;
+
     private void Start()
     {
         isPlaying = true;
+        pausePanel = GameObject.Find("Pause");
+        pausePanel.SetActive(false);
+        pauseBtn = GameObject.Find("Button");
+        pauseBtn.SetActive(false);
     }
 
     // Update is called once per frame
@@ -21,13 +30,37 @@ public class PauseMenu : MonoBehaviour
                 isPlaying = false;
                 Debug.Log("Pause menu");
                 Time.timeScale = 0;
+                // Pause audio
+                AudioListener.pause = true;
+
+                pausePanel.SetActive(true);
+                pauseBtn.SetActive(true);
+                
             }
             else
             {
                 isPlaying = true;
                 Debug.Log("Unpause");
                 Time.timeScale = 1;
+                AudioListener.pause = false;
+
+                pausePanel.SetActive(false);
+                pauseBtn.SetActive(false);
             } 
         }
+    }
+
+    // This function gets called when the Quit button is clicked
+    // The special #if can check if we are in Editor or Build
+    public void QuitGame()
+    {
+        // save any game data here
+#if     UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 }
